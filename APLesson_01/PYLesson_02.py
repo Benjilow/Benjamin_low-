8fb3509a89991e170e70a25890246cc406395724
Benjilow <benjaminlow@san.rr.com>
2016-09-08 09:37:36 -0700

    added files

---
+++ APLesson_01/PYLesson_02.py
@@ -0,0 +1,36 @@
+l = 8;
+w = 5;
+rechArea = l * w;
+
+print (" The area of your rectangle is", rechArea);
+
+
+name = "ProfossorHandsome";
+print (name);
+print (8 + 5);
+
+num1 = 6;
+num2 = 8;
+print (num1 * num2);
+
+num1 = 9;
+num2 = 5;
+print (num1 * num2);
+
+first = "Professor";
+second = "Handsome";
+print (first + second);
+
+kids = 38;
+print("There are " ,kids, " kids in our class.");
+
+address = "492154 Carmel Point";
+City = "San Diego";
+Zip = "92130";
+print (address, City, Zip);
+
+Length = 6;
+Width = 6;
+Height = 7;
+print ( 2*(Width * Length + Height * Length + Height * Width));
+
