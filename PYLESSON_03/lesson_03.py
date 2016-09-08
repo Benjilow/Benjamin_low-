541f989bf2d66b6b24f56e891a31d20630f8cdd9
Benjilow <benjaminlow@san.rr.com>
2016-09-08 10:27:53 -0700

    added lesson_03

---
+++ PYLESSON_03/lesson_03.py
@@ -0,0 +1,10 @@
+david = input("What career do you have currently? ") 
+
+
+print(david , "?!! Oh yeah thats rich!")
+
+
+num1 = int (input ("please enter the money in your bank account: " ))
+num2 = int (input ("please enter the checking account: " ))
+
+print("Inconceivable! Your bank account " , num2-num1, "
